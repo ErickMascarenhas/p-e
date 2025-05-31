38f2df8d5d79babac2df4106354dd2001f3f0be2
ErickMascarenhas <159568716+ErickMascarenhas@users.noreply.github.com>
2025-05-31 15:53:02 -0300

    Delete Alinhamento de DNAs/gerar.py

--- Alinhamento de DNAs/gerar.py
+++
@@ -1,9 +0,0 @@
-import random
-sequencias = 2
-caracteres = 100
-bases = ['A', 'T', 'C', 'G']
-with open('input.txt', 'w') as file:
-    for _ in range(sequencias):
-        length = random.randint(1, caracteres)
-        dna = ''.join(random.choices(bases, k=length))
-        file.write(dna + '\n')
